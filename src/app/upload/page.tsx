e69487e2e32373b3ca9f8d7463bdde01a2569b4c
Anthony Tleiji <anthonytleiji@gmail.com>
2023-11-08 18:12:52 +0200

    Custom Image Upload - Add page - Add simple UI - Add progress bar

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useEdgeStore } from "@/lib/edgestore";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function UploadPage() {
+  const [file, setFile] = useState<File>();
+  const [progress, setProgress] = useState(0);
+  const [urls, setUrls] = useState<{
+    url: string;
+    thumbnailUrl: string | null;
+  }>();
+  const { edgestore } = useEdgeStore();
+
+  return (
+    <div className="flex flex-col items-center m-6 gap-2">
+      <input type="file" onChange={(e) => setFile(e.target.files?.[0])} />
+      <div className="h-[6px] w-44 border rounded overflow-hidden">
+        <div
+          className="h-full bg-white transition-all duration-150"
+          style={{
+            width: `${progress}%`,
+          }}
+        ></div>
+      </div>
+      <button
+        onClick={async () => {
+          if (file) {
+            const res = await edgestore.myPublicImages.upload({
+              file,
+              onProgressChange(progress) {
+                setProgress(progress);
+              },
+            });
+            setUrls({
+              url: res.url,
+              thumbnailUrl: res.thumbnailUrl,
+            });
+          }
+        }}
+        className="bg-white text-black rounded px-2 hover:opacity-80"
+      >
+        Upload
+      </button>
+      {urls?.url && (
+        <Link href={urls.url} target="_blank">
+          URL
+        </Link>
+      )}
+      {urls?.thumbnailUrl && (
+        <Link href={urls.thumbnailUrl} target="_blank">
+          THUMBNAIL
+        </Link>
+      )}
+    </div>
+  );
+}
